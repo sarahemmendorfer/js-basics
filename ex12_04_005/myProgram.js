f498da927f98e62f26f89f3b9ba68c4c63536b2b
Sarah Emmendorfer <sarah_giorgia@hotmail.com>
2021-04-13 10:16:21 -0300

    Exercise of the greatest number

---
+++ ex12_04_005/myProgram.js
@@ -0,0 +1,13 @@
+var n1 = 25;
+var n2 = 20; 
+var n3 = 15;
+
+if(n1 > n2 && n1 > n3){
+    document.write('The largest number is: '+n1);
+}    
+else if((n2 > n1) && (n2 > n3)){
+    document.write('The largest number is: '+n2);
+}
+else{
+    document.write('The largest number is: '+n3);
+}
